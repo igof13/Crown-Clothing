d864869efcf93799024dc9f059ad12f9b140b10b
igor fugagnoli <igof13@gmail.com>
2021-07-05 16:12:19 -0300

    Updates

---
+++ precache-manifest.5b6518cf9542a54adb52e67a055009a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d431372333d25a648a20ee31d1a05c28",
+    "url": "/Crown-Clothing/index.html"
+  },
+  {
+    "revision": "8e2ef234299565ae79b3",
+    "url": "/Crown-Clothing/static/css/main.930b20d2.chunk.css"
+  },
+  {
+    "revision": "3c179b153dcbeb9565df",
+    "url": "/Crown-Clothing/static/js/2.3ee5fded.chunk.js"
+  },
+  {
+    "revision": "8e2ef234299565ae79b3",
+    "url": "/Crown-Clothing/static/js/main.c37bb9c7.chunk.js"
+  },
+  {
+    "revision": "056d1ce45089afc98c4e",
+    "url": "/Crown-Clothing/static/js/runtime~main.1a6c0a33.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/Crown-Clothing/static/media/crown.ed28accb.svg"
+  }
+]);
